6bdd00a3230866b62f2ede1f30bbcb1649a0c041
zengfa <zengf@dtds.com.cn>
2018-05-18 10:21:58 +0800

    单例模式

---
+++ src/main/java/com/study/patterns/singletonPattern/SingletonPatternDemo.java
@@ -0,0 +1,18 @@
+package com.study.patterns.singletonPattern;
+
+public class SingletonPatternDemo {
+
+	public static void main(String[] args) {
+		// illegal construct
+		// Compile Time Error: The constructor SingleObject() is not visible
+		// SingleObject object = new SingleObject();
+
+		// Get the only object available
+		SingleObject object = SingleObject.getInstance();
+
+		// show the message
+		object.showMessage();
+
+	}
+
+}
